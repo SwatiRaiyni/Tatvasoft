dce24c3d37dde394f7806e38e335003756f1c0e1
SwatiRaiyni <swatiraiyani3127@gamil.com>
2021-11-27 17:46:19 +0530

    psd conversion

--- Home_Page/script.js
+++ Home_Page/script.js
@@ -7,3 +7,16 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+let button = document.getElementById("backtotop");
+window.onscroll= function(){
+  fun();
+};  
+function fun(){
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ){
+    button.style.display = "block";
+  }
+  else{
+    button.style.display = "none";
+  }
+}
