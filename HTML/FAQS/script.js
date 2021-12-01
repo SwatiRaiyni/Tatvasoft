723c7caa53711e199aefab48a4f4f5e08ba5b87d
SwatiRaiyni <swatiraiyani3127@gamil.com>
2021-12-01 17:34:51 +0530

    psd to html

--- HTML/FAQS/script.js
+++ HTML/FAQS/script.js
@@ -7,59 +7,59 @@ function myFunction() {
     }
   }
   $(document).ready(function(){
-    $("#demo").on("hide.bs.collapse", function(){
-      $("#btn").html('<img src="assets/vector-smart-object-copy.png">');
-    });
-    $("#demo").on("show.bs.collapse", function(){
-      $("#btn").html('<img src="assets/vector-smart-object.png"> ');
-    });
     $("#demo1").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn1").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo1").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn1").html('<img src="assets/vector-smart-object.png"> ');
     });
-    $("#demo2").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+    $("#demo").on("hide.bs.collapse", function(){
+      $("#btn2").html('<img src="assets/vector-smart-object-copy.png">');
     });
-    $("#demo2").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+    $("#demo").on("show.bs.collapse", function(){
+      $("#btn2").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo3").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn3").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo3").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn3").html('<img src="assets/vector-smart-object.png"> ');
+    });
+    $("#demo9").on("hide.bs.collapse", function(){
+      $("#btn9").html('<img src="assets/vector-smart-object-copy.png">');
+    });
+    $("#demo9").on("show.bs.collapse", function(){
+      $("#btn9").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo4").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn4").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo4").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn4").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo5").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn5").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo5").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn5").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo6").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn6").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo6").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn6").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo7").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn7").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo7").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn7").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo8").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn8").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo8").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn8").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#s").on("hide.bs.collapse", function(){
       $("#btn").html('<img src="assets/vector-smart-object-copy.png">');
@@ -68,52 +68,52 @@ function myFunction() {
       $("#btn").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo19").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn16").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo19").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn16").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo12").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn10").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo12").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn10").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo13").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn11").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo13").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn11").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo14").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn12").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo14").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn12").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo15").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn13").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo15").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn13").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo16").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn14").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo16").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn14").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo17").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn20").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo17").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn20").html('<img src="assets/vector-smart-object.png"> ');
     });
     $("#demo18").on("hide.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object-copy.png">');
+      $("#btn15").html('<img src="assets/vector-smart-object-copy.png">');
     });
     $("#demo18").on("show.bs.collapse", function(){
-      $(".btn").html('<img src="assets/vector-smart-object.png"> ');
+      $("#btn15").html('<img src="assets/vector-smart-object.png"> ');
     });
   });
 
